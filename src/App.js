f750d8b1f9900b9d18f13ba35d9e7f671cce7a6c
azzouza <ratelazzatn@gmail.com>
2023-05-04 15:39:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulaire from "./compenents/Formulaire"
+function App() {
+  return (
+    <div>
+<Formulaire/>
+
+    </div>
+  );
+}
+
+export default App;
